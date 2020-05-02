d0fc95f2beda7d5c4fa1e81b40b0221d3b77b6cd
Alexandre Araujo <aaraujo001@gmail.com>
2020-05-02 16:50:40 +0200

    update lipschitz bound test

--- tests/test_lipschitz_bound.py
+++ tests/test_lipschitz_bound.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import numpy as np
 import scipy as sp
 from scipy.sparse import linalg
+from scipy.sparse.linalg import svds, eigs
 
 import torch
 import torchvision.models as models
@@ -10,48 +11,50 @@ from block_matrices.ops import DoublyBlockToeplitzFromKernel
 from lipschitz_bound import LipschitzBound
 
 
-def test_single_kernel(kernel=None, kshape=None, image_size=None,
-                       padding=None, sample=50, dump_kernel=None):
-  if kernel is None:
-    cout, cin, ksize, ksize = kshape
-    kernel = np.random.randn(cout, cin, ksize, ksize)
+def generate_kernel(kshape, kernel_type):
+  cout, cin, ksize, ksize = kshape
+  kernel = np.random.randn(cout, cin, ksize, ksize)
+  if kernel_type == 'torch':
     kernel = torch.FloatTensor(np.float32(kernel))
-    # kernel = kernel.cuda()
-  cout, cin, ksize, ksize = kernel.shape
+  return kernel
+
+
+def compute_diff(image_size, kshape, padding, kernel_type):
+  kernel = generate_kernel(kshape, kernel_type) 
   A = DoublyBlockToeplitzFromKernel(
-    image_size, kernel.cpu().numpy(), padding=padding, return_sparse=True).generate()
-  sv_max = linalg.svds(
-    A, k=1, which='LM', return_singular_vectors=False)[0]
-
-  lb = LipschitzBound(kernel, padding, sample=sample) 
-  start = datetime.now()
+    image_size, kernel, padding=padding, return_sparse=True).generate()
+  # compute sv max
+  sv_max = svds(A, k=1, which='LM', return_singular_vectors=False)[0]
+  # compute bound
+  lb = LipschitzBound(kernel, padding, sample=200) 
   sv_bound = lb.compute()
-  end = (datetime.now() - start).total_seconds()
-
-  msg = ("Single kernel {}x{}x{}x{}: "
-         "sv_max: {:.5f}, sv_bound: {:.5f}, time: {}")
-  print(msg.format(cout, cin, ksize, ksize, sv_max, sv_bound, end))
+  return sv_max - sv_bound
 
 
-if __name__ == '__main__':
+def loop(kernel_type):
 
-  test_single_kernel(kshape=(1, 1, 3, 3), image_size=29, padding=1, sample=300) 
+  cin, cout = 1, 1
+  for image_size in [30]:
+    for ksize in [3, 5, 7, 9, 11]:
+      for padding in [0, 1, 2, 3, 4]:
+        kshape = [1, 1, ksize, ksize]
+        diff = compute_diff(image_size, kshape, padding, kernel_type)
+        assert diff <= 0.1
 
-  # for ksize in [3, 5, 7, 9, 11]:
-  #   for padding in [0, 1, 2, 3, 4]:
-  #     kshape = [1, 1, ksize, ksize]
-  #     test_single_kernel(kshape=kshape, image_size=60,
-  #                        padding=padding, sample=300)
-  #
-  # for cout in [6, 9, 12]:
-  #   for cin in [6, 9, 12]:
-  #     kshape = [cout, cin, 3, 3]
-  #     test_single_kernel(kshape=kshape, image_size=60,
-  #                        padding=1, sample=200)
+  ksize = 3
+  padding = 1
+  for image_size in [30]:
+    for cout in [6, 9, 12]:
+      for cin in [6, 9, 12]:
+        kshape = [cout, cin, ksize, ksize]
+        diff = compute_diff(image_size, kshape, padding, kernel_type)
+        assert diff <= 0.1
 
-  # shapes = [(4, 4, 3, 3), (20, 20, 3, 3)]
-  # for kshape in shapes:
-  #   test_single_kernel(kshape=kshape, image_size=60,
-  #                      padding=1, sample=200)
+def test_bound_numpy():
+  loop('numpy')
+
+def test_bound_torch():
+  loop('torch')
+
 
 
