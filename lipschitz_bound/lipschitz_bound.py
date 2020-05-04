d45b5cc1aa3a9cfc7eb9cd5665f11a58ac3ccdb5
Alexandre Araujo <aaraujo001@gmail.com>
2020-05-04 18:10:49 +0200

    update Lipchitz Bound computation

--- lipschitz_bound/lipschitz_bound.py
+++ lipschitz_bound/lipschitz_bound.py
@@ -15,29 +15,23 @@ except:
 
 class LipschitzBound:
 
-  def __init__(self, kernel, padding, sample=50, backend='numpy'):
+  def __init__(self, kernel_shape, padding, sample=50, backend='torch',
+               cuda=True):
 
-    self.kernel = kernel
+    self.kernel_shape = kernel_shape
     self.padding = padding
     self.sample = sample
     self.backend = backend
+    self.cuda = cuda
+
+    cout, cin, ksize, _ = kernel_shape
 
     # verify the kernel is square
-    if not kernel.shape[-1] == kernel.shape[-2]:
+    if not kernel_shape[-1] == kernel_shape[-2]:
       raise ValueError("The last 2 dim of the kernel must be equal.")
     # verify if all kernel have odd shape
-    if not kernel.shape[-1] % 2 == 1:
+    if not kernel_shape[-1] % 2 == 1:
       raise ValueError("The dimension of the kernel must be odd.")
-
-    if isinstance(self.kernel, np.ndarray):
-      self.compute_with_torch = False
-      self.compute = self._compute_from_numpy
-    elif isinstance(self.kernel, torch.Tensor):
-      self.compute_with_torch = True
-      self.cuda = self.kernel.is_cuda
-      self.compute = self._compute_from_torch
-    else:
-      raise ValueError('kernel type not recognized.')
 
     # define search space
     x = np.linspace(0, 2*np.pi, num=self.sample)
@@ -45,68 +39,94 @@ class LipschitzBound:
     self.w0 = w[:, 0].reshape(-1, 1)
     self.w1 = w[:, 1].reshape(-1, 1)
 
-    if self.compute_with_torch:
+    # convert search space to torch tensor
+    if self.backend == 'torch':
       self.w0 = torch.FloatTensor(np.float32(self.w0))
       self.w1 = torch.FloatTensor(np.float32(self.w1))
       if self.cuda:
         self.w0 = self.w0.cuda()
         self.w1 = self.w1.cuda()
 
-  def _get_sample(self, *args, **kwargs):
-    if self.compute_with_torch:
-      return self._get_sample_for_torch(*args, **kwargs)
-    return self._get_sample_for_numpy(*args, **kwargs)
+   # create samples
+    if self.backend == 'numpy':
+      p_index = np.arange(-ksize + 1., 1.) + padding
+      H0 = 1j * np.tile(p_index, ksize).reshape(ksize, ksize).T.reshape(-1)
+      H1 = 1j * np.tile(p_index, ksize)
+      self.samples = np.exp(self.w0 * H0 + self.w1 * H1).T
 
-  def _get_sample_for_numpy(self, ksize, pad):
-    p_index = np.arange(-ksize + 1., 1.) + pad
-    H0 = 1j * np.tile(p_index, ksize).reshape(ksize, ksize).T.reshape(-1)
-    H1 = 1j * np.tile(p_index, ksize)
-    return np.exp(self.w0 * H0 + self.w1 * H1).T
+    elif self.backend == 'torch':
+      p_index = torch.arange(-ksize + 1.0, 1.0) + padding
+      H0 = p_index.repeat(ksize).reshape(ksize, ksize).T.reshape(-1)
+      H1 = p_index.repeat(ksize)
+      if self.cuda:
+        H0 = H0.cuda()
+        H1 = H1.cuda()
+      real = torch.cos(self.w0 * H0 + self.w1 * H1).T
+      imag = torch.sin(self.w0 * H0 + self.w1 * H1).T
+      self.samples = (real, imag)
 
-  def _get_sample_for_torch(self, ksize, pad):
-    p_index = torch.arange(-ksize + 1.0, 1.0) + pad
-    H0 = p_index.repeat(ksize).reshape(ksize, ksize).T.reshape(-1)
-    H1 = p_index.repeat(ksize)
-    if self.cuda:
-      H0 = H0.cuda()
-      H1 = H1.cuda()
-    real = torch.cos(self.w0 * H0 + self.w1 * H1).T
-    imag = torch.sin(self.w0 * H0 + self.w1 * H1).T
-    return real, imag
+  def compute(self, *args, **kwargs):
+    if self.backend == 'torch':
+      return self._compute_from_torch(*args, **kwargs)
+    return self._compute_from_numpy(*args, **kwargs)
 
-  def _compute_from_numpy(self):
+  def _compute_from_numpy(self, kernel):
     """Compute the LipGrid Algorithm."""
-    kernel = self.kernel
     pad = self.padding
     cout, cin, ksize, _ = kernel.shape
     if cout > cin:
       kernel = np.transpose(kernel, axes=[1, 0, 2, 3])
       cout, cin = cin, cout
     ker = kernel.reshape(cout, cin, -1)[..., np.newaxis]
-    poly = (ker * self._get_sample(ksize, pad)).sum(axis=2)
+    poly = (ker * self.samples).sum(axis=2)
     poly = np.square(np.abs(poly)).sum(axis=1)
     sv_max = np.sqrt(poly.max(axis=-1).sum())
     return sv_max
 
-  def _compute_from_torch(self):
+  def _compute_from_torch_naive(self, kernel):
     """Compute the LipGrid Algo with Torch"""
-    # only one kernel at a time with torch
-    kernel = self.kernel
     pad = self.padding
     cout, cin, ksize, _ = kernel.shape
     if cout > cin:
       kernel = torch.transpose(kernel, 0, 1)
       cout, cin = cin, cout
-    ker = kernel.reshape(cout, cin, -1)[..., np.newaxis]
-    real, imag = self._get_sample(ksize, pad)
-    poly_real = (ker * real).sum(axis=2)
-    poly_imag = (ker * imag).sum(axis=2)
+
+    ker = kernel.view(cout, cin, -1, 1)
+    real, imag = self.samples
+
+    poly_real = torch.mul(ker, real).sum(axis=2)
+    poly_imag = torch.mul(ker, imag).sum(axis=2)
+
     poly = torch.mul(poly_real, poly_real) + \
         torch.mul(poly_imag, poly_imag)
     poly = poly.sum(axis=1)
     sv_max = torch.sqrt(poly.max(axis=-1)[0].sum())
     return sv_max
- 
 
+  def _compute_from_torch(self, kernel):
+    """Compute the LipGrid Algo with Torch"""
+    pad = self.padding
+    cout, cin, ksize, _ = kernel.shape
+    if cout > cin:
+      kernel = torch.transpose(kernel, 0, 1)
+      cout, cin = cin, cout
+
+    real, imag = self.samples
+    if not kernel.is_contiguous:
+      kernel = kernel.contiguous()
+    if not real.is_contiguous():
+      real = real.contiguous()
+    if not imag.is_contiguous():
+      imag = imag.contiguous()
+    ker = kernel.reshape(cout*cin, -1)
+    poly_real = torch.matmul(ker, real).view(cout, cin, -1)
+    poly_imag = torch.matmul(ker, imag).view(cout, cin, -1)
+
+    poly1 = torch.einsum('ijk,ijk->ik', poly_real, poly_real)
+    poly2 = torch.einsum('ijk,ijk->ik', poly_imag, poly_imag)
+    poly = poly1 + poly2
+
+    sv_max = torch.sqrt(poly.max(axis=-1)[0].sum())
+    return sv_max
 
 
